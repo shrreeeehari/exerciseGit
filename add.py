03c323c8e4d1016c47aaf04aedc7f86dc838233d
Pranav Bhardwaj <50249082+pranav121004@users.noreply.github.com>
2020-09-02 11:57:53 +0530

    addition

---
+++ add.py
@@ -0,0 +1,6 @@
+def add(x, y):
+    return x + y
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("sum of ",num1," and ",num2," is ",add(num1,num2))
