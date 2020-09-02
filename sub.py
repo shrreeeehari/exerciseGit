03a9fd0f734bc506c127ca268b713078beccc671
Pranav Bhardwaj <50249082+pranav121004@users.noreply.github.com>
2020-09-02 12:01:09 +0530

    subtraction

---
+++ sub.py
@@ -0,0 +1,7 @@
+def subtract(x, y):
+    return x - y
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Difference of ",num1," and ",num2," is ",subtract(num1,num2))
